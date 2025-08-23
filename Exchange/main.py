57b4e021935034f87ac3d7432cff7976bfd927de
Sina <Lonew6442@gmail.com>
2025-08-23 16:22:33 +0330

    Exchange Version 1.0.0

---
+++ Exchange/main.py
@@ -0,0 +1,24 @@
+import requests
+
+API_KEY = "82f806e38be1f02696dfc5e8"
+
+base_currency = input("ارز مبدأ را وارد کنید (مثلاً USD): ").upper()
+target_currency = input("ارز مقصد را وارد کنید (مثلاً EUR): ").upper()
+amount = float(input(f"مبلغ را به {base_currency} وارد کنید: "))
+
+url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{base_currency}"
+response = requests.get(url)
+data = response.json()
+
+if data['result'] == 'success':
+    conversion_rates = data['conversion_rates']
+
+    target_rate = conversion_rates[target_currency]
+
+    converted_amount = amount * target_rate
+
+    print("\n--------------------")
+    print(f"{amount} {base_currency} = {converted_amount:.2f} {target_currency}")
+    print("--------------------")
+else:
+    print("خطا: دریافت نرخ تبدیل ناموفق بود.")
